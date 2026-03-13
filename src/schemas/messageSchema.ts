1d419bd06727c5218ef9f4c4712548ed738a556c
Paradise <shrivastavanirmal24@gmail.com>
2026-03-13 18:09:19 +0530

    NextJs setup and Schema Validation

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const signInSchema = z.object({
+    content: z
+    .string()
+    .min(10, {message: "Content must be atleast 10 characters long"})
+    .max(300 ,{message: 'Content must not be more than 10 characters'})
+})
